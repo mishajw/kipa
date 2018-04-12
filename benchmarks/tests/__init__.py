0d362af16c8d02831c5b448f2560dc88286ac86c
Misha Wagner <mishajw@gmail.com>
2018-04-12 20:00:32 +0100

    Add setting logging levels in tests

--- benchmarks/tests/__init__.py
+++ benchmarks/tests/__init__.py
@@ -1,1 +1,8 @@
 from benchmarks.tests import test_search
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("docker").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
