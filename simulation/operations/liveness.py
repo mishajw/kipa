d0101e4dd4fe6d7dbed1a19d455151fff1c2f2c3
Misha Wagner <mishajw@gmail.com>
2019-07-23 18:56:59 +0100

    Add constants in liveness.py and increase retries

--- simulation/operations/liveness.py
+++ simulation/operations/liveness.py
@@ -7,13 +7,16 @@ from simulation.networks import Network
 
 log = logging.getLogger(__name__)
 
+NUM_ATTEMPTS = 10
+ATTEMPT_WAIT_SECS = 2
+
 
 def ensure_all_alive(network: Network, backend: Backend) -> None:
     log.debug("Ensuring all nodes in the network are alive")
 
     ids = network.ids()
 
-    for attempt in range(3):
+    for attempt in range(NUM_ATTEMPTS):
         commands = [CliCommand(node_id, ["list-neighbours"]) for node_id in ids]
         results = backend.run_commands(commands)
         ids = [
@@ -25,11 +28,13 @@ def ensure_all_alive(network: Network, backend: Backend) -> None:
             log.debug("All nodes ensured alive")
             return
         log.debug(
-            "At attempt %d, still %d nodes not responding. Sleeping 1s",
+            "At attempt %d/%d, still %d nodes not responding. Sleeping %d seconds.",
             attempt + 1,
+            NUM_ATTEMPTS,
             len(ids),
+            ATTEMPT_WAIT_SECS,
         )
-        time.sleep(1)
+        time.sleep(ATTEMPT_WAIT_SECS)
 
     raise AssertionError(
         f"{len(ids)} nodes still didn't reply after all attempts"
