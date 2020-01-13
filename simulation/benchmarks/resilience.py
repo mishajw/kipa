30ea4856779d1d20d09befdfaab48fa439b6fc31
Misha Wagner <mishajw@gmail.com>
2020-01-13 19:30:15 +0000

    Fix resilience benchmark.

--- simulation/benchmarks/resilience.py
+++ simulation/benchmarks/resilience.py
@@ -3,9 +3,12 @@ import random
 from pathlib import Path
 
 from simulation.benchmarks import SuccessSpeedBenchmark
-from simulation.networks import Network
+from simulation.networks import Network, Node
 
 log = logging.getLogger(__name__)
+
+REMOVE_FLAGS = {"neighbours_size", "neighbour_gc_enabled", "search_breadth"}
+MALICIOUS_FEATURES = {"use-random-response", "use-protobuf", "use-tcp", "use-unix-socket"}
 
 MALICIOUS_PROBABILITIES = [x / 10 for x in range(10)]
 
@@ -18,8 +21,16 @@ class ResilienceBenchmark(SuccessSpeedBenchmark):
 
     def get_network(self, network: Network, malicious_probability: float) -> Network:
         return network.map_nodes(
-            lambda n: n.replace(additional_features=random.random() < malicious_probability)
+            lambda n: _to_malicious_node(n) if random.random() < malicious_probability else n
         )
 
     def format_parameter(self, parameter: float) -> str:
         return str(parameter * 100)
+
+
+def _to_malicious_node(node: Node) -> Node:
+    return node.replace(
+        clear_default_features=True,
+        additional_features=frozenset(MALICIOUS_FEATURES),
+        daemon_args={k: v for k, v in node.daemon_args.items() if k not in REMOVE_FLAGS},
+    )
